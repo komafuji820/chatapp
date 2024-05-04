2c30827ef58234848329645d6903546707b53a2d
komafuji820 <tfukae25@gmail.com>
2024-05-04 11:11:59 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,4 +3,19 @@ class RoomsController < ApplicationController
     @room = Room.new
     @users = User.all.where.not(id: current_user.id)
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      @users = User.all.where.not(id: current_user.id)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name, user_ids:[])
+  end
 end
