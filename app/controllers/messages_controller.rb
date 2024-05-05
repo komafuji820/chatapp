9d3e5355b4c3bf4f8e73737d903b2658d90485aa
komafuji820 <tfukae25@gmail.com>
2024-05-05 21:22:01 +0900

    画像表示の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,7 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-      redirect_to room_message_path(@message.room.id)
+      redirect_to room_messages_path(@message.room.id)
     else
       @rooms = current_user.rooms
       @messages = @room.messages.includes(:user)
